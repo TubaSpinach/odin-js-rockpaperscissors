0afa18d5987411aeb062f1fa93093381732854a8
Liam Tait <142124471+TubaSpinach@users.noreply.github.com>
2023-10-22 11:15:11 -0400

    feat: add play round function

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
 function getComputerChoice() {
     let randInt = Math.floor(Math.random() * 3 + 1);
-    console.log(randInt)
     let choice = (randInt === 1) ? "Rock" : (randInt === 2) ? "Paper" : "Scissors";
-    return choice
+    return choice;
+}
+
+function playRound(pChoice,cChoice) {
+    let cleanPChoice = pChoice.toLowerCase();
+    let cleanCChoice = cChoice.toLowerCase();
+    let loseTo;
+    let winTo;
+    let winStatement = "You win!";
+    let lossStatement = "You lose :(";
+    let tieStatement = "It's a tie? o.O";
+    switch (cleanPChoice) {
+        case "rock":
+            loseTo = "paper";
+            winTo = "scissors";
+        break;
+
+        case "paper":
+            loseTo = "scissors";
+            winTo = "rock";
+        break;
+
+        case "scissors":
+            loseTo = "rock";
+            winTo = "paper";
+        break;
+
+        default:
+            console.log("please type 'rock', 'paper', or 'scissors'");
+    }
+    if(cleanCChoice == loseTo) {
+        return lossStatement;
+    } else if (cleanCChoice == winTo) {
+        return winStatement;
+    } else {
+        return tieStatement;
+    }
 }
